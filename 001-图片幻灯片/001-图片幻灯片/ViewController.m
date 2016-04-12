d7a5ca1610631f9859052f9b45c954fd1fd442e8
apple <apple@appledeMacBook-Pro-2.local>
2016-04-12 14:46:37 +0800

    幻灯片demo初始化

---
+++ 001-图片幻灯片/001-图片幻灯片/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  001-图片幻灯片
+//
+//  Created by apple on 16/4/12.
+//  Copyright © 2016年 gengzhuo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
